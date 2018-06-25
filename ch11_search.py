730d66187e92c35877801f454e8db81c68e41225
Angela Shen <angelashen01@gmail.com>
2018-06-25 11:19:16 -0700

    search 11.5-11.8 6-21-2018

--- ch11_search.py
+++ ch11_search.py
@@ -1,3 +1,8 @@
+import math
+import collections
+import random
+import operator
+
 # 11.1 SEARCH A SORTED ARRAY FOR THE FIRST OCCURANCE OF K
 def find_first_k(arr, k):
 	k_candidate_indices = []
@@ -83,7 +88,7 @@ def int_square_root(n):
 			return prev_sqrt
 		prev_sqrt = num
 
-def square_root(k):
+def square_root_BOOK(k):
 	left, right = 0, k
 	while left <= right:
 		mid = (left + right) // 2 
@@ -94,9 +99,114 @@ def square_root(k):
 			right = mid - 1
 	return left - 1 
 
+# 11.5 COMPUTE THE REAL SQUARE ROOT
+def real_square_root_BOOK(x):
+	left, right = (x, 1.0) if x < 1.0 else (1.0, x)
+
+	while not math.isclose(left, right):
+		mid = 0.5 * (left + right)
+		mid_squared = mid * mid
+		if mid_squared > x:
+			right = mid
+		else:
+			left = mid
+	return left
+
+# 11.6 SEARCH IN A 2D SORTED ARRAY
+def search_2d_array(arr, k):
+	row, col = 0, len(arr[0]) - 1 
+	while col >= 0 and row < len(arr):
+		if k > arr[row][col]:
+			row += 1 
+		elif k == arr[row][col]:
+			return row, col
+		else:
+			col -= 1 
+	return -1 
+
+def matrix_search_BOOK(A, x):
+	row, col = 0, len(A[0])-1
+	while row < len(A) and col >= 0:
+		if A[row][col] == x:
+			return True
+		elif A[row][col] < x:
+			row += 1
+		else:
+			col -= 1
+	return False 
+
+# 11.7 COMPUTE THE MIN AND MAX SIMULTANEOUSLY
+def get_min_max(arr):
+	min, max = float('inf'), float('-inf')
+	for val in arr:
+		if val < min:
+			min = val
+		elif val > max:
+			max = val
+	return min, max
+
+MinMax = collections.namedtuple('MinMax', ('smallest', 'largest'))
+def find_min_max_BOOK(A):
+	def min_max(a, b):
+		return MinMax(a, b) if a < b else MinMax(b, a)
+
+	if len(A) <= 1 :
+		return MinMax(A[0], A[0])
+
+	global_min_max = min_max(A[0], A[1])
+	for i in range(2, len(A)-1, 2):
+		local_min_max = min_max(A[i], A[i+1])
+		global_min_max = MinMax(
+			min(global_min_max.smallest, local_min_max.smallest),
+			max(global_min_max.largest, local_min_max.largest))
+
+	if len(A) % 2:
+		global_min_max = MinMax(
+						min(global_min_max.smallest, A[-1]),
+						max(global_min_max.largest, A[-1]))
+
+	return global_min_max
+
+# 11.8 FIND THE KTH LARGEST ELEMENT
+def find_kth_largest(k, A):
+	def find_kth(comp):
+		def partition_around_pivot(left, right, pivot_idx):
+			pivot_value = A[pivot_idx]
+			new_pivot_idx = left
+			A[pivot_idx], A[right] = A[right], A[pivot_idx]
+			for i in range(left, right):
+				if comp(A[i], pivot_value):
+					A[i], A[new_pivot_idx] = A[new_pivot_idx], A[i]
+					new_pivot_idx += 1
+			A[right], A[new_pivot_idx] = A[new_pivot_idx], A[right]
+			return new_pivot_idx
+
+		left, right = 0, len(A) - 1
+		while left <= right:
+			pivot_idx = random.randint(left, right)
+			new_pivot_idx = partition_around_pivot(left, right, pivot_idx)
+			if new_pivot_idx == k - 1:
+				return A[new_pivot_idx]
+			elif new_pivot_idx > k - 1:
+				right = new_pivot_idx - 1
+			else:
+				left = new_pivot_idx + 1
+	return find_kth(operator.gt)
 
 if __name__ == "__main__":
-	print(int_square_root(1342))
+	matrix = [[-1,2,4,4,6],
+			  [1,5,5,9,21],
+			  [3,6,6,9,22],
+			  [3,6,8,10,24],
+			  [6,8,9,12,25],
+			  [8,10,12,13,40]]
+
+	print(search_2d_array(matrix, 8))
+	print(matrix_search_BOOK(matrix, 8))
+	# arr = [1,4,7,3,9,2,5,34543,23,2,5,8,3,2432,674,-1]
+	# print(get_min_max(arr))
+	# print(find_min_max_BOOK(arr))
+	# print(int_square_root(1342))
 	# arr = [378, 478, 550, 631, 103, 203, 220, 234, 279, 368]
 	# print(smallest_in_cyclic_array(arr))
 	# arr = [-2,0,2,2,4,5,6]
