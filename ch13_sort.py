a0ec67eed7f37120bdd6913a61dd53352055ec8a
Angela Shen <angelashen01@gmail.com>
2018-07-03 15:03:00 -0700

    ch 13 sorting 13.5 - 13.6 6-27-2018

--- ch13_sort.py
+++ ch13_sort.py
@@ -1,3 +1,5 @@
+import collections
+
 # 13.1 COMPUTE THE INTERSECTION OF TWO SORTED ARRAYS
 def find_intersection(arr1, arr2):
 	def is_present(x):
@@ -140,9 +142,95 @@ def smallest_nonconstructible_value_BOOK(A):
 		max_constructible_value += a
 	return max_constructible_value + 1
 
+# 13.5 RENDER A CALENDAR
+Event = collections.namedtuple('Event', ('start_time', 'end_time'))
+
+def render_calendar(events):
+	max_overlapping_events = 1
+	sorted_events = sorted(events, key=lambda x: x.start_time)
+	for i in range(len(sorted_events)-1):
+		overlapping_events = 1
+		event_idx = i + 1
+		while (sorted_events[event_idx].start_time >=sorted_events[i].start_time
+		  and sorted_events[event_idx].start_time <= sorted_events[i].end_time
+		  and event_idx < len(sorted_events) - 1):
+			overlapping_events += 1
+			event_idx += 1
+		max_overlapping_events = max(max_overlapping_events, overlapping_events)
+	return max_overlapping_events
+
+Event_BOOK = collections.namedtuple('Event', ('start', 'finish'))
+Endpoint = collections.namedtuple('Endpoint', ('time', 'is_start'))
+
+def find_max_simultaneous_events_BOOK(A):
+	E =  ([Endpoint(event.start_time, True) for event in A] + 
+		  [Endpoint(event.end_time, False) for event in A])
+	E.sort(key=lambda e: (e.time, not e.is_start))
+
+	max_num_simultaneous_events, num_simultaneous_events = 0, 0
+	for e in E:
+		if e.is_start:
+			num_simultaneous_events += 1
+			max_num_simultaneous_events = max(num_simultaneous_events, max_num_simultaneous_events)
+		else:
+			num_simultaneous_events -= 1
+	return max_num_simultaneous_events
+
+# 13.6 MERGING INTERVALS
+Interval = collections.namedtuple('Interval', ('start', 'end'))
+def merge_intervals(intervals, new_interval):
+	result = []
+	new_start, new_end = None, None
+	for interval in intervals:
+		if interval.end < new_interval.start or interval.start > new_interval.end:
+			result.append(interval)
+		else:
+			if interval.start < new_interval.start and interval.end < new_interval.end:
+				new_start = interval.start
+			elif interval.end > new_interval.start and interval.end > new_interval.end:
+				new_end = interval.end
+	if not new_start:
+		new_start = new_interval.start
+	if not new_end:
+		new_end = new_interval.end
+	result.append(Interval(new_start, new_end))
+	return result
+
+def add_interval_BOOK(disjoint_intervals, new_interval):
+	i, result = 0, []
+
+	while (i < len(disjoint_intervals) and 
+		   new_interval.start > disjoint_intervals[i].end):
+		result.append(disjoint_intervals[i])
+		i += 1
+
+	while (i < len(disjoint_intervals) and
+		   new_interval.end >= disjoint_intervals[i].start):
+		new_interval = Interval(
+			min(new_interval.start, disjoint_intervals[i].start),
+			max(new_interval.end, disjoint_intervals[i].end))
+		i += 1
+
+	return result + [new_interval] + disjoint_intervals[i:]
+
 if __name__ == "__main__":
-	A = [1,1,1,1,1,5,10,25]
-	print(smallest_nonconstructible_value(A))
+	intervals = [Interval(-4, -1), Interval(0, 2), Interval(3, 6), Interval(11, 12), Interval(14, 17)]
+	new_interval = Interval(1, 8)
+	print(merge_intervals(intervals, new_interval))
+	print(add_interval_BOOK(intervals, new_interval))
+	# events = [Event(7, 10), Event(8, 9), Event(12, 17), Event(13, 13.5), Event(7.5, 8.5), Event(20, 23), 
+	# 			Event(13, 14), Event(15, 15.5), Event(14, 17)]
+
+	# print(find_max_simultaneous_events_BOOK(events))
+	# print(render_calendar(events))
+	# E =  ([Endpoint(event.start_time, True) for event in events] + 
+	# 	  [Endpoint(event.end_time, False) for event in events])
+	# E.sort(key=lambda e: (e.time, not e.is_start))
+	# print(E)
+	
+	# print(render_calendar(events))
+	# A = [1,1,1,1,1,5,10,25]
+	# print(smallest_nonconstructible_value(A))
 	# names = [Name("Angi", "Shen"), Name("Babs", "McBab"), Name("Babs", "McCool"), Name("Austin", "Stone")]
 	# dups_removed = remove_first_name_duplicates(names)
 	# for name in dups_removed:
