27a5fa1776d9ef6158893304aa3ae7f215e8ca3f
Angela Shen <angelashen01@gmail.com>
2018-07-13 11:57:40 -0700

    ch 14 BST 14.8-14.11 7-2-2018

--- ch14_binary_search_trees.py
+++ ch14_binary_search_trees.py
@@ -203,22 +203,157 @@ def find_closest_elements(sorted_arrays):
 # 14.7
 # this one's dumb :(
 
+# 14.8 
+def k_most_visited(stream, k):
+    sorted_dict = SortedDict()
+    for page in stream:
+        if page not in sorted_dict:
+            sorted_dict[page] = 1
+        else:
+            sorted_dict[page] += 1
+
+    return [sorted_dict.popitem(index=0) for x in range(k)]
+
+#14.9 BUILD A MINIMUM HEIGHT BST FROM A SORTED ARRAY
+
+def build_min_height_bst(arr):
+    def build_min_bst_helper(start, end):
+        if start >= end:
+            return None
+        mid = (start + end) // 2
+        return TreeNode(arr[mid], build_min_bst_helper(start, mid), build_min_bst_helper(mid+1, end))
+    return build_min_bst_helper(0, len(arr))
+
+def build_max_height_bst(arr):
+    def build_max_height_bst_helper(idx):
+        if idx >= len(arr) - 1:
+            return None
+        return TreeNode(arr[idx], left=None, right=build_max_height_bst_helper(idx+1))
+    return build_max_height_bst_helper(0)
+
+def get_max_tree_height(tree):
+    if not tree:
+        return 0
+    return max(get_max_tree_height(tree.left)+1, get_max_tree_height(tree.right)+1)
+
+def build_min_height_bst_from_sorted_array(A):
+    def build_min_height_bst_from_sorted_subarray(start, end):
+        if start >= end:
+            return None
+        return BinaryTreeNode(A[mid],
+                              build_min_height_bst_from_sorted_subarray(start, mid),
+                              build_min_height_bst_from_sorted_subarray(mid+1, end))
+    return build_min_height_bst_from_sorted_array(0, len(A))
+
+# 14.10 INSERTION AND DELETION IN A BST
+class BinaryTreeNode:
+    def __init__(self):
+        self._root = None
+
+    def insert(self, key):
+        if not self._root:
+            self._root = BinaryTreeNode(key)
+        else:
+            parent = None
+            curr = self._root
+            while curr:
+                parent = curr
+                if curr.data == key:
+                    return False
+                elif key < curr.data:
+                    curr = curr.left
+                else:
+                    curr = curr.right
+
+            if key < parent.data:
+                parent.left = BinaryTreeNode(key)
+            else:
+                parent.right = BinaryTreeNode(key)
+            return True
+
+    def delete(self, key):
+        curr = self._root
+        parent = None
+        while curr and curr.data != key:
+            parent = curr
+            curr = curr.left if curr.data > key else curr.right
+
+        if not curr:
+            return False
+
+        key_node = curr
+        if key_node.right:
+            r_key_node = key_node.right
+            r_parent = key_node
+            while r_key_node.left:
+                r_parent = r_key_node
+                r_key_node = r_key_node.left
+            key_node.data = r_key_node.data
+            if r_parent.left == key_node:
+                r_parent.left = r_key_node.right
+            else:
+                r_parent.right = r_key_node.right
+        else:
+            if self._root == key_node:
+                self._root = key_node.left
+            else:
+                if parent.left == key_node:
+                    parent.left = key_node.left
+                else:
+                    parent.right = key_node.left
+        return True
+
+# 14.11 TEST IF THREE BST NODES ARE TOTALLY ORDERED
+def pair_includes_ancestor_and_descendent_of_m(a, b, mid):
+    seach_0, search_1 = a, b
+
+    while (search_0 is not b and search_0 is not mid and 
+           search_1 is not a and search_1 is not mid and
+           (search_0 or search_1)):
+        if search_0:
+            search_0 = (search_0.left if search_0.data >
+                        middle.data else search_0.right)
+        if search_1:
+            search_1 = (search_1.left if search_1.data >
+                        middle.data else search_1.right)
+
+    if ((search_0 is not middle and search_1 is not middle) or
+            search_0 is b or search_1 is a):
+        return False
+
+    def search_target(source, target):
+        while source and source is not target:
+            source = source.left if source.data > target.data else source.right
+        return source is target
+
+    return search_target(
+        middle, b if search_0 is middle else a)
 
 if __name__ == "__main__":
-    node11 = TreeNode(5)
-    node10 = TreeNode(3)
-    node9 = TreeNode(1)
-    node8 = TreeNode(-1)
-    node7 = TreeNode(12)
-    node6 = TreeNode(8)
-    node5 = TreeNode(4, node10, node11)
-    node4 = TreeNode(0, node8, node9)
-    node3 = TreeNode(10, node6, node7)
-    node2 = TreeNode(2, node4, node5)
-    root = TreeNode(6, node2, node3)
+    # node11 = TreeNode(5)
+    # node10 = TreeNode(3)
+    # node9 = TreeNode(1)
+    # node8 = TreeNode(-1)
+    # node7 = TreeNode(12)
+    # node6 = TreeNode(8)
+    # node5 = TreeNode(4, node10, node11)
+    # node4 = TreeNode(0, node8, node9)
+    # node3 = TreeNode(10, node6, node7)
+    # node2 = TreeNode(2, node4, node5)
+    # root = TreeNode(6, node2, node3)
 
-    sorted_arrays = [[5,10,15], [3,6,8,12,14], [8,16,24]]
-    print(find_closest_elements(sorted_arrays))
+    # stream = ['c', 'd', 'e', 'a', 'a', 'a', 'a', 'c', 'd', 'c', 'd', 'e', 'g', 'f', 'a', 'd', 'c']
+    # print(k_most_visited(stream, 3))
+
+    arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+    min_height_tree = build_min_height_bst(arr)
+    max_height_tree = build_max_height_bst(arr)
+    print(get_max_tree_height(min_height_tree))
+    print(get_max_tree_height(max_height_tree))
+
+
+    # sorted_arrays = [[5,10,15], [3,6,8,12,14], [8,16,24]]
+    # print(find_closest_elements(sorted_arrays))
     # traversal_order = [8,4,2,1,3,6,5,7,12,10,9,11,14,13,15]
     # tree = tree_from_preorder_traversal(traversal_order)
     # print(preorder_recursive(tree))
